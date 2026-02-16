a74e47795297fa11d61112e1a4bbb2c89eeab334
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-02-16 07:34:29 +0300

    Homepage list fix

---
+++ app/(guest)/_components/featured-section.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from 'react'
+import ProductCardComponent from './product-card';
+import { ProductCard } from '@/lib/models/products';
+
+
+type FeaturedSection = {
+    id: string
+    name: string
+    bg_title_color: string
+    title_color: string
+    products: ProductCard[]
+}
+
+
+export interface FeaturedSectionProps {
+    items: any;
+}
+
+const FeaturedSection = ({ items }: FeaturedSectionProps) => {
+
+    console.log(items)
+
+  return (
+    <div>
+          {items?.map((featured: FeaturedSection) => (
+              <div key={featured.id} className="bg-white rounded shadow flex flex-col mb-5">
+                  <div
+                      style={{ backgroundColor: featured.bg_title_color, color: featured.title_color }}
+                      className="px-4 py-1">
+                      {featured.name}
+                  </div>
+
+                  <div className="py-2">
+                      {featured?.products?.length > 0 && (
+                          <div className="grid md:grid-cols-5 gap-5">
+                              {featured?.products.map((product: ProductCard) => (
+
+                                  <ProductCardComponent key={product.id} product={product} />
+
+                              ))}
+                          </div>
+                      )}
+                  </div>
+              </div>
+          ))}
+    </div>
+  )
+}
+
+export default FeaturedSection
--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -2,16 +2,11 @@ import React from 'react'
 import SidebarMenu from './_components/sidebar-menu'
 import HeroSection from './_components/hero-section'
 import { ApiRequests } from '@/lib/requests/api_requests';
-import ProductCardComponent from './_components/product-card';
-import { ProductCard } from '@/lib/models/products';
 
-type FeaturedSection = {
-  id: string
-  name: string
-  bg_title_color: string
-  title_color: string
-  products: ProductCard[]
-}
+import { ProductCard } from '@/lib/models/products';
+import FeaturedSection from './_components/featured-section';
+
+
 
 
 export const metadata = {
@@ -39,27 +34,7 @@ const Homepage = async () => {
 
 
       <div className="mt-5">
-        {featuredProducts?.map((featured: FeaturedSection) => (
-          <div key={featured.id} className="bg-white rounded shadow flex flex-col mb-5">
-            <div
-              style={{ backgroundColor: featured.bg_title_color, color: featured.title_color }}
-              className="px-4 py-1">
-              {featured.name}
-            </div>
-
-            <div className="py-2">
-              {featured?.products?.length > 0 && (
-                <div className="grid md:grid-cols-5 gap-5">
-                  {featured?.products.map((product: ProductCard) => (
-                    
-                    <ProductCardComponent key={product.id} product={product} />
-                    
-                  ))}
-                </div>
-              )}
-            </div>
-          </div>
-        ))}
+        <FeaturedSection items={featuredProducts} />
       </div>
 
     </div>
