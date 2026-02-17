333e732338239eb13bdd70a772a2cc86a8231c41
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-02-17 12:52:15 +0300

    fixes

--- app/(dashboard)/admin/products/[productID]/page.tsx
+++ app/(dashboard)/admin/products/[productID]/page.tsx
@@ -1,39 +1,13 @@
-import React from 'react';
-import { ApiRequests } from '@/lib/requests/api_requests';
-import PageClient from './pageClient';
-import { auth } from '@/auth';
 
-type ProductDetailsProps = {
-  params: Promise<{
-    productID: string;
-  }>;
-}
+"use client";
 
-const ProductDetails = async ({ params }: ProductDetailsProps) => {
-  const { productID } = await params;
-  const session = await auth();
 
-  if(!session?.sessionToken) return;
-  
+import React from 'react'
 
-  const product = await ApiRequests.get(`superadmin/products/products/${productID}/`, session?.sessionToken);
-  const [categories, brands, featuresList] = await Promise.all([
-    ApiRequests.get("superadmin/products/category-children/", session.sessionToken),
-    ApiRequests.get("products/brands/", session.sessionToken),
-    ApiRequests.get("products/features/", session.sessionToken),
-  ]);
-          
- 
-  
-  if(!product){
-    return (
-      <p>Loading product data .....</p>
-    );
-  }
-
+const page = () => {
   return (
-    <PageClient product={product} categories={categories} brands={brands} features={featuresList} />
+    <div>page</div>
   )
 }
 
-export default ProductDetails
+export default page
