ea0789e8556f1ed2e3c84a2130f36f0ee71bea0e
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-02-17 14:02:27 +0300

    fixes product details

--- app/(guest)/[category]/[product]/product-client.tsx
+++ app/(guest)/[category]/[product]/product-client.tsx
@@ -52,31 +52,31 @@ const ProductClient = ({ productData }: ProductClientProps) => {
                     <div className="grid grid-cols-1 md:grid-cols-12 gap-3 bg-white rounded p-3">
                         <div className="md:col-span-4">
                             <div className="relative h-full md:h-[270px]">
-                                <Image src={product?.thumbnail} alt={`QUZA Leading Online Shop in Kenya ${product?.name} available, ${product?.category} products available, ${product?.brand} available`} fill className="object-contain" />
+                                <Image src={productData?.thumbnail} alt={`QUZA Leading Online Shop in Kenya ${productData?.name} available, ${productData?.category} products available, ${productData?.brand} available`} fill className="object-contain" />
                             </div>
                         </div>
                         <div className="md:col-span-8 relative">
 
                             <div className="absolute top-0 right-0">
-                                <Button variant="ghost" onClick={() => onAddToWish(product)} className="cursor-pointer"><HeartIcon /></Button>
+                                <Button variant="ghost" onClick={() => onAddToWish(productData)} className="cursor-pointer"><HeartIcon /></Button>
                             </div>
 
                             <div className="pb-3 pe-5">
-                                <h1 className="text-xl">{product?.name}</h1>
-                                <p className="text-slate-500 text-sm">Brand: {product?.brand}</p>
-                                <p className="pb-5 text-slate-500 text-xs">{product?.category}</p>
+                                <h1 className="text-xl">{productData?.name}</h1>
+                                <p className="text-slate-500 text-sm">Brand: {productData?.brand}</p>
+                                <p className="pb-5 text-slate-500 text-xs">{productData?.category}</p>
                             </div>
                             
 
-                            <p className="font-bold text-qsecondary mb-6">KSh {parseInt(product?.default_variant?.price).toLocaleString()}</p>
+                            <p className="font-bold text-qsecondary mb-6">KSh {parseInt(productData?.default_variant?.price).toLocaleString()}</p>
 
                             <div className='pb-5'>
-                                <p className="text-sm text-slate-500 line-clamp-4 text-ellipsis">{product?.description}</p>
+                                <p className="text-sm text-slate-500 line-clamp-4 text-ellipsis">{productData?.description}</p>
                             </div>
                             <div className="flex space-x-3.5">
-                                <Button className='bg-qprimary cursor-pointer' onClick={() => onAddToCart(product)}><ShoppingCartIcon /> Add to Cart</Button>
+                                <Button className='bg-qprimary cursor-pointer' onClick={() => onAddToCart(productData)}><ShoppingCartIcon /> Add to Cart</Button>
 
-                                <Link href={`https://wa.me/254119439544/?text=Hello Quza, is ${product.name}, KSh ${parseInt(product?.default_variant?.price)} available?`} target="_blank">
+                                <Link href={`https://wa.me/254119439544/?text=Hello Quza, is ${productData.name}, KSh ${parseInt(productData?.default_variant?.price)} available?`} target="_blank">
                                     <Button className='bg-green-600 hover:bg-green-800 cursor-pointer'><FaWhatsapp /> WhatsApp Enquiry</Button>
                                 </Link>
                                 
