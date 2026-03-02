5227158545b1b8596df263e96c4bec93c6175dc9
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-03-02 07:34:47 +0300

    Fix ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: "quza-web",
+            script: "node_modules/next/dist/bin/next",
+            args: "start -p 3000",
+            cwd: "/qm/frontend",
+            instances: 1,
+            exec_mode: "fork",
+            max_memory_restart: "500M",
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
+};
