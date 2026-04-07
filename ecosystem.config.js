9f2880f4913e43b680bfc665e3791a80b14c5540
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-04-07 12:32:37 +0300

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,11 @@ module.exports = {
         {
             name: "quza-web",
             script: "node_modules/next/dist/bin/next",
-            args: "start -p 3000",
-            cwd: "/qm/frontend",
+            args: ["start", "-p", "3000"],
+            cwd: "/home/peter/qm/frontend",
             instances: 1,
             exec_mode: "fork",
+            interpreter: "node",
             max_memory_restart: "500M",
             env: {
                 NODE_ENV: "production",
@@ -14,3 +15,5 @@ module.exports = {
         },
     ],
 };
+
+
