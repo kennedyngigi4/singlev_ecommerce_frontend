0d35f52b046e59bddc69c0917229d6094d68dc4d
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-04-10 10:04:21 +0300

    update pm2 cwd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: "quza-web",
             script: "node_modules/next/dist/bin/next",
-            args: ["start", "-p", "3000"],
+            args: ["start", "-p", "3000", "-H", "127.0.0.1"],
             cwd: "/home/quza.co.ke/public_html",
             instances: 1,
             exec_mode: "fork",
@@ -15,5 +15,3 @@ module.exports = {
         },
     ],
 };
-
-
